bdf4a4cc26bdba83715eec824d29b458f7c31f54
Sdg877 <sdrakegill@gmail.com>
2025-06-19 15:33:52 +0100

    Cors config updated to allow requests from frontend.

--- server/index.js
+++ server/index.js
@@ -4,18 +4,26 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
 
 const app = express();
-app.use(cors());
+
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true,
+}));
+
 app.use(express.json());
 
-app.use('/', authRoutes); 
+app.use('/', authRoutes);
+app.use('/', taskRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
