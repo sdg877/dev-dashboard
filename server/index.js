6ddae6b5a5e4e26565254ac621b3244b9cd98e59
Sdg877 <sdrakegill@gmail.com>
2025-06-19 15:35:55 +0100

    Cors config updated to allow requests from frontend.

--- server/index.js
+++ server/index.js
@@ -3,27 +3,30 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const authRoutes = require('./routes/auth');
-const taskRoutes = require('./routes/tasks');
+const authRoutes = require('./routes/auth'); 
+const taskRoutes = require('./routes/tasks'); 
 
 const app = express();
 
-
 app.use(cors({
   origin: 'http://localhost:3000', 
-  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
 
+
+app.get('/', (req, res) => res.send('API is running'));
 app.use('/', authRoutes);
 app.use('/', taskRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT || 5000, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
-  })
-  .catch((err) => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+  app.listen(process.env.PORT || 5000, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+  );
+}).catch((err) => console.error('MongoDB connection error:', err));
