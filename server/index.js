233aa64126cc528b80a0fd8273cd6d720340e799
Sdg877 <sdrakegill@gmail.com>
2025-06-23 05:02:39 +0100

    Resolving login issues with cors policy.

--- server/index.js
+++ server/index.js
@@ -1,32 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const authRoutes = require('./routes/auth'); 
-const taskRoutes = require('./routes/tasks'); 
+const authRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/tasks");
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("API is running"));
+app.use("/", authRoutes);
+app.use("/", taskRoutes);
 
-app.get('/', (req, res) => res.send('API is running'));
-app.use('/', authRoutes);
-app.use('/', taskRoutes);
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(process.env.PORT || 5000, () =>
-    console.log(`Server running on port ${process.env.PORT}`)
-  );
-}).catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
