dfc6ef86628e825eea57f65a26bcbcbcb30b4574
Sdg877 <sdrakegill@gmail.com>
2025-06-24 06:36:12 +0100

    Update to import of GitHubSearch page to fix error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Tasks from './pages/Tasks';
-import GitHubSearch from './GitHubSearch';
+import GitHubSearch from './pages/GitHubSearch';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
