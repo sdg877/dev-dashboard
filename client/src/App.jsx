ec359ea7a451f7e20fb53fb7b1a0c9056054dd42
Sdg877 <sdrakegill@gmail.com>
2025-06-25 13:31:13 +0100

    Routes added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Tasks from './pages/Tasks';
-import GitHubSearch from './pages/GitHubSearch';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import Dashboard from './Dashboard';
+import Login from './Login';
+import Register from './Register';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
-function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/`)
-      .then((res) => setMessage(res.data))
-      .catch((err) => console.error(err));
-  }, []);
-
+const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/github" element={<GitHubSearch />} />
-          <Route path="/" element={<Navigate to="/dashboard" />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
