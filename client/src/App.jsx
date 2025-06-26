a252c65b78ee8615aaab948fafee64948cae68c7
Sdg877 <sdrakegill@gmail.com>
2025-06-26 17:33:56 +0100

    Updated to include navbar.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,17 +7,19 @@ import Settings from './pages/Settings';
 import Dashboard from './Dashboard';
 import Login from './Login';
 import Register from './Register';
+import Navbar from './components/Navbar'; // new import
 
 const App = () => {
   return (
     <Router>
+      <Navbar /> 
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} /> 
       </Routes>
     </Router>
   );
