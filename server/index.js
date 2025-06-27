23674925cf6199f2cf7205dcdbc67dfcd2e5896f
Sdg877 <sdrakegill@gmail.com>
2025-06-27 04:49:01 +0100

    Update to github route import.

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
 const taskRoutes = require("./routes/tasks");
-const githubRoutes = require('./routes/githubRoutes');
-
+const githubRoutes = require("./routes/github"); 
 
 const app = express();
 
@@ -21,10 +20,10 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("API is running"));
+
 app.use("/", authRoutes);
 app.use("/", taskRoutes);
-app.use("/", githubRoutes);
-
+app.use("/", githubRoutes); 
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -34,7 +33,7 @@ mongoose
   .then(() => {
     console.log("MongoDB connected");
     app.listen(process.env.PORT || 5000, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
     );
   })
   .catch((err) => console.error("MongoDB connection error:", err));
